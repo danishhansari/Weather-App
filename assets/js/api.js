091d02a70e56c13171d708d193a28a973c2874b2
Danish <80449860+danishhansari@users.noreply.github.com>
2023-05-31 14:43:32 +0530

    http to https

--- assets/js/api.js
+++ assets/js/api.js
@@ -26,13 +26,13 @@ export const url = {
         return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
     airPollution(lat, lon){
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
     reverseGeo(lat,lon){
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
     // param {string} query Search query eg: "londan", "mumbai"
     geo(query){
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
 }
